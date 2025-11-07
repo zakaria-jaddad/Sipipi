89e7b11df9b61197237bb5aa99825c3b91bef632
zakaria jaddad <zakaria.jaddad26@gmail.com>
2025-11-07 20:06:14 +0100

    Imta hayssali hachi, Lah yltef bina

---
+++ day_2/ex00/Fixed.cpp
@@ -0,0 +1,24 @@
+#include "Fixed.hpp"
+#include <cmath>
+
+Fixed::Fixed() : _rawValue(0) { std::cout << "Default constructor called\n"; }
+
+Fixed &Fixed::operator=(const Fixed &other) {
+  std::cout << "Copy assignment operator called\n";
+  if (this != &other) {
+    this->_rawValue = other.getRawBits();
+  }
+  return *this;
+}
+
+Fixed::Fixed(const Fixed &other) {
+  // this->_rawValue = other.getRawBits();
+  *this = other;
+}
+
+int Fixed::getRawBits(void) const {
+  std::cout << "getRawBits member function called\n";
+  return this->_rawValue;
+}
+
+Fixed::~Fixed() { std::cout << "Destructor called\n"; }
