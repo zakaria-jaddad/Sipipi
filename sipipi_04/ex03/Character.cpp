0a426b47eb0fe0f1cc7e23b1a8ec3a03279c88ea
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2025-12-04 02:06:41 +0100

    L3fo: Had 42 Ktrat Lhadra

--- sipipi_04/ex03/Character.cpp
+++ sipipi_04/ex03/Character.cpp
@@ -2,18 +2,73 @@
 
 Character::Character() {
   this->_name = "Default Character Name";
+  this->_currentIndex = 0;
+  this->_currentUnequippedIndex = 0;
   for (int i = 0; i < 4; i++) {
     this->_inventory[i] = NULL;
+    this->_unequippedInventory[i] = NULL;
   }
 }
 
 Character::Character(std::string &name) {
   this->_name = name;
+  this->_currentIndex = 0;
+  this->_currentUnequippedIndex = 0;
   for (int i = 0; i < 4; i++) {
     this->_inventory[i] = NULL;
+    this->_unequippedInventory[i] = NULL;
   }
 }
 
 Character::Character(const Character &other) { *this = other; }
 
-Character
+Character &Character::operator=(const Character &other) {
+  if (this == &other)
+    return *this;
+  this->_name = other._name;
+  for (int i = 0; i < this->_currentIndex; i++) {
+    delete this->_inventory[i];
+  }
+  this->_currentIndex = other._currentIndex;
+  this->_currentUnequippedIndex = other._currentUnequippedIndex;
+  for (int i = 0; i < 4; i++) {
+    this->_inventory[i] = other._inventory[i]->clone();
+    this->_unequippedInventory[i] = other._unequippedInventory[i];
+  }
+}
+
+Character::~Character() {
+
+  for (int i = 0; i < this->_currentIndex; i++) {
+    delete this->_inventory[i];
+  }
+  for (int i = 0; i < this->_currentUnequippedIndex; i++) {
+    delete this->_unequippedInventory[i];
+  }
+}
+
+std::string const &Character::getName() const { return this->_name; }
+
+void Character::setName(std::string name) { this->_name = name; }
+
+void Character::equip(AMateria *m) {
+  if (m == NULL)
+    return;
+  if (this->_currentIndex > 3)
+    return;
+
+  this->_inventory[this->_currentIndex++] = m;
+}
+
+void Character::unequip(int idx) {
+  if (idx > 3 || idx < 0)
+    return;
+  if (this->_inventory[idx] == NULL)
+    return;
+
+  if (this->_currentUnequippedIndex > 3)
+    return;
+  this->_unequippedInventory[this->_currentUnequippedIndex++] =
+      this->_inventory[idx];
+  this->_inventory[idx] = NULL;
+}
--- sipipi_04/ex03/Character.hpp
+++ sipipi_04/ex03/Character.hpp
@@ -6,16 +6,22 @@
 
 class Character : public ICharacter {
 private:
+  int _currentIndex;
+  int _currentUnequippedIndex;
   std::string _name;
   AMateria *_inventory[4];
+  AMateria *_unequippedInventory[4];
 
 public:
   Character();
   Character(std::string &name);
   Character(const Character &other);
   Character &operator=(const Character &other);
+  ~Character();
 
+  void setName(std::string name);
   virtual std::string const &getName() const;
+
   virtual void equip(AMateria *m);
   virtual void unequip(int idx);
   virtual void use(int idx, ICharacter &target);
